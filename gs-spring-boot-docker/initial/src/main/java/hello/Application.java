54891a2e647ceb45c0c2c78026f7eb755e48f84f
YongBhinKim <coji67@gmail.com>
2022-05-06 17:56:20 +0900

    main 내부 server추가

--- gs-spring-boot-docker/initial/src/main/java/hello/Application.java
+++ gs-spring-boot-docker/initial/src/main/java/hello/Application.java
@@ -2,16 +2,35 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
+import java.io.*; 
+import java.net.*;
 
 @SpringBootApplication
 @RestController
 public class Application {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		SpringApplication.run(Application.class, args);
+
+		String clientSentence, serverSentence; 
+		ServerSocket welcomeSocket = new ServerSocket(10000);
+
+		while(true){
+			Socket connectionSocket = welcomeSocket.accept(); 
+			System.out.println("client-server 연결완료\n");
+			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream())); 
+			DataOutputStream  outToClient = 
+			new DataOutputStream(connectionSocket.getOutputStream()); 
+			clientSentence = inFromClient.readLine();
+			serverSentence = clientSentence;
+				//===============================================
+				// 받은 clientSentence를 복호화
+				// 보낼 serverSentence를 암호화
+				//===============================================
+			System.out.println("clientSentence = " + clientSentence);
+			outToClient.writeBytes(serverSentence + '\n'); 
+		}
 	}
-	
 }
