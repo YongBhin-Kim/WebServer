aed172febc551365a0dd7c5227d63951651f8a55
YongBhinKim <coji67@gmail.com>
2022-06-03 03:06:05 +0900

    JNI를 이용한 암/복호화 통신[Client]

--- Client/Client.java
+++ Client/Client.java
@@ -1,37 +1,107 @@
 import java.io.*; 
 import java.net.*; 
 class Client { 
-
+    public static String stringToHex(String s) {
+        String result = "";
+    
+        for (int i = 0; i < s.length(); i++) {
+          result += String.format("%02X ", (int) s.charAt(i));
+        }
+    
+        return result;
+    }
+    public native int[] Enc(int[] tmp);
+    public native int[] Dec(int[] tmp);
+    public native int addInteger(int num1, int num2);
     public native String input(String str);
+    // public native String Dec(String str);
     public static void main(String argv[]) throws Exception { 
 
         // Socket clientSocketForSpring = new Socket("localhost", 8080); // Docker Image 다운 시에는 필요 없음.
-
-         // C에서 stdin으로 메시지를 입력받아 암호화한 후 가져와서 Server로 보낸다.
-        Client h = new Client();
-        String clientSentence = h.input("Input : "); String receivedSentence;
-        System.out.println(clientSentence);
-
         Socket clientSocket = new Socket("localhost", 10000);
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        String[] PT = stringToHex(inFromUser.readLine()).split(" ");
 
-        // loop:
-        // while(true) {
-        // BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); 
+        int[] PT2 = new int[PT.length];
+        for (int i = 0; i < PT.length; i++) {
+            PT2[i] = Integer.parseInt(PT[i]);
+        } // PT2 배열 JNI로 넘기기!!
+
+        Client h = new Client();
+        // int[] t = new int[PT.length];
+        int[] out = h.Enc(PT2);
+        StringBuilder inttoSB = new StringBuilder();
+        // out = int[128]
+        // out = String
+        // send(out)
+        for (int i = 0; i < 128; i++) {
+            inttoSB.append(String.valueOf(out[i]));
+            inttoSB.append(" ");
+        }
+        String clientSentence = inttoSB.toString();
+
+        //  C에서 stdin으로 메시지를 입력받아 암호화한 후 가져와서 Server로 보낸다.        
+
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream()); 
-        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); 
-        // System.out.println("Input : ");
-        // clientSentence = inFromUser.readLine();
+        outToServer.writeBytes(clientSentence + "\n");
 
-        outToServer.writeBytes(clientSentence + '\n'); 
-        receivedSentence = inFromServer.readLine();
-        //===============================================
-        // 보낼 clientSentence를 암호화
-        // 받은 receivedSentence를 복호화
-        //===============================================
-        System.out.println("From Server: " + receivedSentence);
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); 
+        String receivedSentence = inFromServer.readLine();
+        // System.out.println("[받은 메시지 : java]\n");
+        // System.out.println(receivedSentence + "\n");
+        String[] CT = receivedSentence.split(" ");
+        int[] CT2 = new int[CT.length]; // 잘못됨
+        for (int i = 0; i < CT.length; i++) {
+            CT2[i] = Integer.parseInt(CT[i]);
+        } // PT2 배열 JNI로 넘기기!!
+
+
+        Client j = new Client();
+        // int[] t = new int[PT.length];
+        int[] out2 = j.Dec(CT2); // int (사실 hex)
+        StringBuilder hexStr = new StringBuilder();
+        for (int i = 0; i < 128; i++) {
+            hexStr.append(String.valueOf(out2[i]) + " ");
+        }
+        String tmp = hexStr.toString(); // hex (string)
+
+        StringBuilder realhex = new StringBuilder();
+        String[] tmp2 = tmp.split(" "); // hex (string)
+        for (int i = 0; i < 128; i++) {
+            realhex.append( (char) Integer.parseInt(tmp2[i], 16));
+        }
+        String decrypted = realhex.toString();
+        System.out.println("[복호화]\n" + decrypted);
+
+
+        // for (int i = 0; i < 128; i++) {
+
         // }
+        // String decryptedSentence = inttoASCII.toString();
+        // System.out.println(decryptedSentence);
+        // System.out.println("[복호문]");
+        // System.out.println(decryptedSentence);
+        // out = String
+        // for (int i = 0; i < 128; i++) {
+        //     inttoSB2.append(String.valueOf(out2[i]));
+        //     inttoSB2.append(" ");
+        // }
+        // String serverSentence = inttoSB2.toString();
+
+        // System.out.println(serverSentence);
+
+
+
+        // System.out.println("From Server: " + receivedSentence);
         clientSocket.close(); 
-        // clientSocketForSpring.close();
+
+        // //===============================================
+        // // 보낼 clientSentence를 암호화
+        // // 받은 receivedSentence를 복호화
+        // //===============================================
+        // // }
+        // // clientSocketForSpring.close();
+
     } 
     static {
         System.loadLibrary("BlockCipher");
