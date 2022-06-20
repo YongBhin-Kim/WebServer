32053ca11b9fdd78ed0ffcc9a4404162c89ef7c1
YongBhinKim <coji67@gmail.com>
2022-06-20 23:41:52 +0900

    JNI를 이용한 암/복호화 통신[Client]

--- Client/Client.java
+++ Client/Client.java
@@ -22,35 +22,35 @@ class Client {
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); // 입력값 받기
         int[] PT = stringToInt(inFromUser.readLine());
         Client h = new Client();
-        int[] out = h.Enc(PT); // int 배열 JNI 이용하여 C로 넘겨 암호화
+        int[] out = h.Enc(PT);                              // int 배열 JNI 이용하여 C로 넘겨 암호화
         StringBuilder inttoSB = new StringBuilder();
         for (int i = 0; i < 128; i++) {
-            inttoSB.append(String.valueOf(out[i])); // int배열을 stringbuilder으로 합체
+            inttoSB.append(String.valueOf(out[i]));         // "val val val val"
             if (i < 127)
                 inttoSB.append(" ");
         } 
-        String clientSentence = inttoSB.toString(); // stringbuilder -> string 변환
+        String clientSentence = inttoSB.toString();         // "val val val val"
 
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream()); 
-        outToServer.writeBytes(clientSentence + "\n"); // send encrypted input string to server
+        outToServer.writeBytes(clientSentence + "\n");      // send encrypted input string to server
 
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); 
-        String receivedSentence = inFromServer.readLine(); // received encrypted string sentence from server
-        String[] CT = receivedSentence.split(" ");
-        int[] CT2 = new int[CT.length]; // string.split() -> int array
+        String receivedSentence = inFromServer.readLine();  // received encrypted string sentence from server
+        String[] CT = receivedSentence.split(" ");   // ["val"] ["val"] ["val"] ["val"]
+        int[] CT2 = new int[CT.length];                     
         for (int i = 0; i < CT.length; i++) {
-            CT2[i] = Integer.parseInt(CT[i]);
+            CT2[i] = Integer.parseInt(CT[i]);               // [val] [val] [val] [val]
         }
 
 
         Client j = new Client();
-        int[] out2 = j.Dec(CT2); // int 배열 JNI 이용하여 C로 넘겨 복호화 [80] [80] [80] [80]
+        int[] out2 = j.Dec(CT2);                            // [val] [val] [val] [val]
         StringBuilder inttoStr = new StringBuilder();
         for (int i = 0; i < PT.length; i++) {
-            inttoStr.append((char) out2[i]);   // 80 -> a
+            inttoStr.append((char) out2[i]);                // [char] [char] [char] [char]
         }
-        String tmp = inttoStr.toString(); // "80 80 80 80 80"
-        System.out.println("[복호화]\n" + tmp);
+        String tmp = inttoStr.toString();                   // "char char char char" 
+        System.out.println("[복호화]\n" + tmp);              
         clientSocket.close(); 
     } 
     static {
