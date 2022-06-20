816f49512f9ab0d1507a66336bd19ebc1fe9b1de
YongBhinKim <coji67@gmail.com>
2022-06-20 23:32:32 +0900

    JNI를 이용한 암/복호화 통신[Client]

--- Client/Client.java
+++ Client/Client.java
@@ -1,11 +1,15 @@
 import java.io.*; 
 import java.net.*; 
 class Client { 
-    public static String stringToHex(String s) {
-        String result = "";
+    public static int[] stringToInt(String s) {
+        int[] result = new int[128];
+        
     
         for (int i = 0; i < s.length(); i++) {
-          result += String.format("%02X ", (int) s.charAt(i));
+            result[i] = (int) s.charAt(i);
+        }
+        for (int i = s.length(); i < 128; i++){
+            result[i] = 0;
         }
     
         return result;
@@ -16,18 +20,14 @@ class Client {
 
         Socket clientSocket = new Socket("localhost", 10000); // Client 포트 : 10000
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); // 입력값 받기
-        String[] PT = stringToHex(inFromUser.readLine()).split(" "); 
-
-        int[] PT2 = new int[PT.length];
-        for (int i = 0; i < PT.length; i++) {
-            PT2[i] = Integer.parseInt(PT[i]); // string -> int
-        } 
+        int[] PT = stringToInt(inFromUser.readLine());
         Client h = new Client();
-        int[] out = h.Enc(PT2); // int 배열 JNI 이용하여 C로 넘겨 암호화
+        int[] out = h.Enc(PT); // int 배열 JNI 이용하여 C로 넘겨 암호화
         StringBuilder inttoSB = new StringBuilder();
         for (int i = 0; i < 128; i++) {
             inttoSB.append(String.valueOf(out[i])); // int배열을 stringbuilder으로 합체
-            inttoSB.append(" ");
+            if (i < 127)
+                inttoSB.append(" ");
         } 
         String clientSentence = inttoSB.toString(); // stringbuilder -> string 변환
 
@@ -44,20 +44,13 @@ class Client {
 
 
         Client j = new Client();
-        int[] out2 = j.Dec(CT2); // int 배열 JNI 이용하여 C로 넘겨 복호화
-        StringBuilder hexStr = new StringBuilder();
-        for (int i = 0; i < 128; i++) {
-            hexStr.append(String.valueOf(out2[i]) + " "); // int array : [val] [val] [val] -> string : "[val] [val] [val]"
+        int[] out2 = j.Dec(CT2); // int 배열 JNI 이용하여 C로 넘겨 복호화 [80] [80] [80] [80]
+        StringBuilder inttoStr = new StringBuilder();
+        for (int i = 0; i < PT.length; i++) {
+            inttoStr.append((char) out2[i]);   // 80 -> a
         }
-        String tmp = hexStr.toString(); // hex (string)
-
-        StringBuilder realhex = new StringBuilder();
-        String[] tmp2 = tmp.split(" "); // hex (string)
-        for (int i = 0; i < 128; i++) {
-            realhex.append( (char) Integer.parseInt(tmp2[i], 16));
-        }
-        String decrypted = realhex.toString();
-        System.out.println("[복호화]\n" + decrypted);
+        String tmp = inttoStr.toString(); // "80 80 80 80 80"
+        System.out.println("[복호화]\n" + tmp);
         clientSocket.close(); 
     } 
     static {
