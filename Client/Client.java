237f3bb178c944fb8268b4337972ca09974a8864
YongBhinKim <coji67@gmail.com>
2022-06-20 14:42:41 +0900

    JNI를 이용한 암/복호화 통신

--- Client/Client.java
+++ Client/Client.java
@@ -5,7 +5,7 @@ class Client {
         String result = "";
     
         for (int i = 0; i < s.length(); i++) {
-          result += String.format("%02X ", (int) s.charAt(i));
+          result += String.format("%02d ", (int) s.charAt(i));
         }
     
         return result;
@@ -20,12 +20,21 @@ class Client {
         // Socket clientSocketForSpring = new Socket("localhost", 8080); // Docker Image 다운 시에는 필요 없음.
         Socket clientSocket = new Socket("localhost", 10000); // Client 포트 : 10000
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        String[] PT = stringToHex(inFromUser.readLine()).split(" "); 
-
-        int[] PT2 = new int[PT.length];
+        String[] PT = stringToHex(inFromUser.readLine()).split(" ");
+        // System.out.println("PT[0] = ", PT[0]);
+        // System.out.println("PT[0] = ", PT[1]);
+        // System.out.println("PT[0] = ", PT[2]);
+        int[] PT2 = new int[128];
         for (int i = 0; i < PT.length; i++) {
+            // System.out.println("PT2", PT);
+            // char c = PT[i].charAt(0);
             PT2[i] = Integer.parseInt(PT[i]);
         } // int 배열 JNI를 이용하여 C로 넘기기
+        if(PT.length < 127) {
+            for (int i = PT.length; i < 128; i++) {
+                PT2[i] = 0;
+            }
+        }
 
         Client h = new Client();
         int[] out = h.Enc(PT2); // out = int[128]
@@ -44,28 +53,33 @@ class Client {
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); 
         String receivedSentence = inFromServer.readLine();
         String[] CT = receivedSentence.split(" ");
-        int[] CT2 = new int[CT.length];
+        int[] CT2 = new int[128];
         for (int i = 0; i < CT.length; i++) {
             CT2[i] = Integer.parseInt(CT[i]);
+            // System.out.println("%02d", CT2[i]);
         } // PT2 배열 JNI로 넘기기!!
-
-
+        // if (CT.length < 128 ) {
+        //     for (int i = CT.length; i < 128; i++) {
+        //         CT
+        //     }
+        // }
+        
         Client j = new Client();
         // int[] t = new int[PT.length];
-        int[] out2 = j.Dec(CT2); // int (사실 hex)
+        int[] out2 = j.Dec(CT2); // int 
         StringBuilder hexStr = new StringBuilder();
         for (int i = 0; i < 128; i++) {
             hexStr.append(String.valueOf(out2[i]) + " ");
         }
         String tmp = hexStr.toString(); // hex (string)
-
+        // System.out.println("[복호화(string)]\n" + tmp);
         StringBuilder realhex = new StringBuilder();
         String[] tmp2 = tmp.split(" "); // hex (string)
-        for (int i = 0; i < 128; i++) {
-            realhex.append( (char) Integer.parseInt(tmp2[i], 16));
+        for (int i = 0; i < PT.length; i++) {
+            realhex.append( (char) Integer.parseInt(tmp2[i])); //tmp2[i], 16
         }
         String decrypted = realhex.toString();
-        System.out.println("[복호화]\n" + decrypted);
+        System.out.println("[복호화(문자열)]\n" + decrypted);
 
 
         // for (int i = 0; i < 128; i++) {
